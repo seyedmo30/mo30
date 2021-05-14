ad13e975198d5cf94840dc3d0c3a21bf6eb1fbcb
hp <seyed.mo30@gmail.com>
2021-05-14 01:00:29 -0700

    add media root to urls

--- index/urls.py
+++ index/urls.py
@@ -23,10 +23,10 @@ urlpatterns = [
 
 ]
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-if settings.DEBUG:
+# if settings.DEBUG:
 
-    
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL,
+                        document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
